61e5ac207d876c37093b6bdaecb1369808a33030
Andy Hayes <andythemanhayes@hotmail.com>
2016-08-08 22:53:29 -0600

    fish show layout completed, need to do logic to get the right one

---
+++ client/templates/today_index.js
@@ -0,0 +1,53 @@
+Template.todayIndex.onCreated( function() {
+  this.currentTab = new ReactiveVar( "codFish" );
+});
+
+Template.todayIndex.helpers({
+  // whichFish: function() {
+  //   var date = new Date()
+  //   var today = moment(date).format("MM.DD.YYYY");
+
+  //   return Votes.find()
+  // },
+  whichFish: function() {
+    return Template.instance().currentTab.get();
+  },
+
+  todayDate: function() {
+    var date = new Date()
+    var today = moment(date).format("MM.DD.YYYY");
+    var month = dateToText(moment(date).format("MM"));
+    dateString  = dateToText(moment(date).format("MM")) + ' ' + moment(date).format("DD") + ', ' + moment(date).format("YYYY");
+    // var today = date.toString();
+    return dateString;
+  }, 
+
+})
+
+ function dateToText(month) {
+    if(month == '01'){
+      return "January";
+    } else if(month == '02'){
+      return "February";
+    } else if(month == '03'){
+      return "March";
+    }else if(month == '04'){
+      return "April";
+    } else if(month == '05'){
+      return "May";
+    } else if(month == '06'){
+      return "June";
+    } else if(month == '07'){
+      return "July";
+    } else if(month == '08'){
+      return "August";
+    } else if(month == '09'){
+      return "September";
+    } else if(month == '10'){
+      return "October";
+    } else if(month == '11'){
+      return "November";
+    } else if(month == '12'){
+      return "December";
+    }
+  }
