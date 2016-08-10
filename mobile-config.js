217fbb621d0f35448a9d95b0e2f4905d7f62bfa0
Andy Hayes <andythemanhayes@hotmail.com>
2016-08-10 15:45:48 -0600

    adds app config, just making sure it works

---
+++ mobile-config.js
@@ -0,0 +1,21 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'com.example.matt.uber',
+  name: 'Fish',
+  description: 'Something smells fishy.. ',
+  author: 'Andrew Hayes',
+  email: 'contact@example.com',
+  website: 'http://example.com'
+});
+// Set up resources such as icons and launch screens.
+// App.icons({
+//   'iphone': 'icons/icon-60.png',
+//   'iphone_2x': 'icons/icon-60@2x.png',
+//   // ... more screen sizes and platforms ...
+// });
+App.launchScreens({
+  'iphone': 'fish/fish_background.png',
+  'iphone_2x': 'fish/fish_background.png',
+  // ... more screen sizes and platforms ...
+});
