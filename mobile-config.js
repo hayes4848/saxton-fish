9959968d9f5c581121c0234a163ae0f5216df3bb
Andy Hayes <andythemanhayes@hotmail.com>
2016-08-14 23:43:52 -0600

    adds icon images

--- mobile-config.js
+++ mobile-config.js
@@ -10,12 +10,14 @@ App.info({
 });
 // Set up resources such as icons and launch screens.
 App.icons({
-  'iphone': 'public/PNGs/icon60.png',
+  // 'iphone': 'public/PNGs/icon60.png',
   'iphone_2x': 'public/PNGs/icon60.png',
+  'iphone_3x': 'public/PNGs/icon180.png',
   // ... more screen sizes and platforms ...
 });
 App.launchScreens({
   'iphone6': 'public/fish/fish_background.png',
-  'iphone_2x': 'public/fish/fish_background.png',
+  'iphone_2x': 'public/fish/fish_background640x960.png',
+  'iphone6p_portrait': 'public/fish/fish_background1242x2208.png',
   // ... more screen sizes and platforms ...
 });
